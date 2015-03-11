3662fc366de1f5f525ce1270db4f67e3737f2195
garfieldjty <jtygarfield@gmail.com>
2015-03-11 17:58:59 +0800

    add user controller param validate

--- src/main/java/com/sjtu/icare/common/config/ErrorConstants.java
+++ src/main/java/com/sjtu/icare/common/config/ErrorConstants.java
@@ -30,7 +30,16 @@ public class ErrorConstants {
 	public static final int GERO_AREA_INSERT_PARAM_INVALID = 200002;
 	public static final int GERO_AREA_INSERT_SERVICE_FAILED = 200003;
 
-
+	// user service constants
+	public static final int USER_UPDATE_INFO_PARAM_INVALID = 1000001;
+	public static final int USER_FOR_ID_NOT_FOUND = 1000002;
+	public static final int USER_UPDATE_INFO_SERVICE_ERROR = 1000003;
+	public static final int USER_INFO_GET_PAGE__SERVICE_ERROR = 1000004;
+	public static final int USER_UPDATE_PASSWORD_PARAM_INVALID = 1000005;
+	public static final int USER_UPDATE_PASSWORD_SERVICE_ERROR = 1000006;
+	public static final int USER_UPDATE_ROLE_PARAM_INVALID = 1000007;
+	public static final int USER_UPDATE_ROLE_SERVICE_ERROR = 1000008;
+	public static final int USER_DELETE_SERVICE_ERROR = 1000009;
 	
 	
 	
--- src/main/java/com/sjtu/icare/modules/sys/webservice/UserController.java
+++ src/main/java/com/sjtu/icare/modules/sys/webservice/UserController.java
@@ -20,9 +20,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import com.sjtu.icare.common.config.ErrorConstants;
 import com.sjtu.icare.common.persistence.Page;
 import com.sjtu.icare.common.utils.BasicReturnedJson;
+import com.sjtu.icare.common.utils.ParamUtils;
 import com.sjtu.icare.common.web.rest.MediaTypes;
+import com.sjtu.icare.common.web.rest.RestException;
 import com.sjtu.icare.modules.sys.entity.Privilege;
 import com.sjtu.icare.modules.sys.entity.Role;
 import com.sjtu.icare.modules.sys.entity.User;
@@ -39,22 +42,43 @@ import com.sjtu.icare.modules.sys.utils.UserUtils;
 */
 @RestController
 @RequestMapping("/user")
-public class UserController {
+public class UserController{
 	private static Logger logger = Logger.getLogger(UserController.class);
 	
 	@Autowired
 	private SystemService systemService;
 	
+	/**
+	 * 获取用户列表（超管）（分页）
+	 * @param page
+	 * @param limit
+	 * @return
+	 */
 	@RequestMapping(value = "", method = RequestMethod.GET, produces = MediaTypes.JSON_UTF_8)
 	public Map<String, Object> getUserInfoList(
 			@RequestParam int page,
 			@RequestParam int limit
 			){
 		BasicReturnedJson result = new BasicReturnedJson();
+		
 		Page<User> userPage = new Page<User>(page, limit);
-		userPage = systemService.findUser(userPage, new User());
-		List<User> userList = userPage.getList();
-		int total = (int) userPage.getCount();
+		
+		// get page from service
+		List<User> userList;
+		int total;
+		try {
+			userPage = systemService.findUser(userPage, new User());
+			userList = userPage.getList();
+			total = (int) userPage.getCount();
+		} catch (Exception e) {
+			String message = "#" + ErrorConstants.USER_INFO_GET_PAGE__SERVICE_ERROR + "#\n" + 
+					"USER_INFO_GET_PAGE__SERVICE_ERROR:\n" +
+					"\n";
+			logger.error(message);
+			throw new RestException(HttpStatus.INTERNAL_SERVER_ERROR, message);
+		}
+		
+		
 		for (User user : userList){
 			if (user != null){
 				result.addEntity(getUserMapFromUser(user));
@@ -62,122 +86,264 @@ public class UserController {
 				result.setError(HttpStatus.OK.name());
 				result.setTotal(total);
 			}else {
-				result.setStatus(HttpStatus.NOT_FOUND.value());
-				result.setError(HttpStatus.NOT_FOUND.name());
-				break;
+				String message = "#" + ErrorConstants.USER_FOR_ID_NOT_FOUND + "#\n" + 
+						"USER_IN_LIST_FOUND:\n" +
+						"\n";
+				logger.error(message);
+				throw new RestException(HttpStatus.INTERNAL_SERVER_ERROR, message);
 			}
 		}
 		return result.getMap();
 	}
 	
+	/**
+	 * 获取uid用户详细信息
+	 * @param uid
+	 * @return
+	 */
 	@RequestMapping(value = "/{uid}", method = RequestMethod.GET, produces = MediaTypes.JSON_UTF_8)
 	public Map<String, Object> getUserInfoFromUserId(@PathVariable("uid") int uid){
+		
 		BasicReturnedJson result = new BasicReturnedJson();
-		User user = UserUtils.get(uid);
-		if (user != null){			
-			result.addEntity(getUserMapFromUser(user));
-			result.setStatus(HttpStatus.OK.value());
-			result.setError(HttpStatus.OK.name());
-		}else {
-			result.setStatus(HttpStatus.NOT_FOUND.value());
-			result.setError(HttpStatus.NOT_FOUND.name());
-		}
+		
+		User user = getUserFromId(uid);
+		
+		result.addEntity(getUserMapFromUser(user));
+		result.setStatus(HttpStatus.OK.value());
+		result.setError(HttpStatus.OK.name());
+		
 		return result.getMap();
 	}
 	
+	/**
+	 * 更新用户详细信息
+	 * @param uid
+	 * @param inJson
+	 * @return
+	 */
 	@RequestMapping(value = "/{uid}", method = RequestMethod.PUT, produces = MediaTypes.JSON_UTF_8)
 	public Map<String, Object> updateUserInfoFromUserId(
 			@PathVariable("uid") int uid,
-			@RequestBody String requestString
+			@RequestBody String inJson
 			){
-		JSONObject requestJson = JSON.parseObject(requestString);
-		String name = (String) requestJson.get("name");
-		String photoUrl = (String) requestJson.get("photo_url");
-		User user = UserUtils.get(uid);
-		BasicReturnedJson result = new BasicReturnedJson();
-		if (user != null){	
-			user.setName(name);
-			user.setPhotoUrl(photoUrl);	
-			systemService.updateUserInfo(user);
-			result.addEntity(getUserMapFromUser(user));
-			result.setStatus(HttpStatus.OK.value());
-			result.setError(HttpStatus.OK.name());
-		}else {
-			result.setStatus(HttpStatus.NOT_FOUND.value());
-			result.setError(HttpStatus.NOT_FOUND.name());
+		Map<String, Object> requestBodyParamMap = ParamUtils.getMapByJson(inJson, logger);
+		String name;
+		String photoUrl;
+		
+		// 输入参数检查
+		try {
+			name = (String) String.valueOf(requestBodyParamMap.get("name"));
+			photoUrl = (String) String.valueOf(requestBodyParamMap.get("photo_url"));
+						
+			if (name == null || photoUrl == null || name.equals("null") || photoUrl.equals("null"))
+				throw new Exception();
+			
+			// 此处可添加url的验证方法
+			
+		} catch(Exception e) {
+			String message = "#" + ErrorConstants.USER_UPDATE_INFO_PARAM_INVALID + "#\n" + 
+					"USER_UPDATE_INFO_PARAM_INVALID\n" +
+					"[name=" + requestBodyParamMap.get("name") + "]" +
+					"[photoUrl=" + requestBodyParamMap.get("photoUrl") + "]" +
+					"\n";
+			logger.error(message);
+			throw new RestException(HttpStatus.BAD_REQUEST, message);
 		}
+		
+		User user = getUserFromId(uid);
+		
+		BasicReturnedJson result = new BasicReturnedJson();
+		
+		user.setName(name);
+		user.setPhotoUrl(photoUrl);	
+		
+		try {
+			systemService.updateUserInfo(user);
+		} catch (Exception e) {
+			String message = "#" + ErrorConstants.USER_UPDATE_INFO_SERVICE_ERROR + "#\n" + 
+					"USER_UPDATE_INFO_SERVICE_ERROR:\n" +
+					"[uid=" + uid + "]" +
+					"\n";
+			logger.error(message);
+			throw new RestException(HttpStatus.INTERNAL_SERVER_ERROR, message);
+		}
+		
+		result.addEntity(getUserMapFromUser(user));
+		result.setStatus(HttpStatus.OK.value());
+		result.setError(HttpStatus.OK.name());
+		
 		return result.getMap();
 	}
 	
+	/**
+	 * 更新用户密码
+	 * @param uid
+	 * @param inJson
+	 * @return
+	 */
 	@RequestMapping(value = "/{uid}/password", method = RequestMethod.PUT, produces = MediaTypes.JSON_UTF_8)
 	public Map<String, Object> updateUserPasswordFromUserId(
 			@PathVariable("uid") int uid,
-			@RequestBody String requestString
+			@RequestBody String inJson
 			){
-		JSONObject requestJson = JSON.parseObject(requestString);
-		String password = (String) requestJson.get("password");
-		User user = UserUtils.get(uid);
-		BasicReturnedJson result = new BasicReturnedJson();
-		if (user != null){	
-			systemService.updatePasswordById(uid, password);
-			result.setStatus(HttpStatus.OK.value());
-			result.setError(HttpStatus.OK.name());
-		}else {
-			result.setStatus(HttpStatus.NOT_FOUND.value());
-			result.setError(HttpStatus.NOT_FOUND.name());
+		Map<String, Object> requestBodyParamMap = ParamUtils.getMapByJson(inJson, logger);
+		String password;
+		
+		// 输入参数检查
+		try {
+			password = (String) String.valueOf(requestBodyParamMap.get("password"));
+						
+			if (password == null || password.equals("null"))
+				throw new Exception();
+			
+			// 此处可添加密码长度的验证方法
+			
+		} catch(Exception e) {
+			String message = "#" + ErrorConstants.USER_UPDATE_PASSWORD_PARAM_INVALID + "#\n" + 
+					"USER_UPDATE_PASSWORD_PARAM_INVALID\n" +
+					"[password=" + requestBodyParamMap.get("password") + "]" +
+					"\n";
+			logger.error(message);
+			throw new RestException(HttpStatus.BAD_REQUEST, message);
 		}
+		
+		User user = getUserFromId(uid);
+		
+		BasicReturnedJson result = new BasicReturnedJson();
+			
+		try {
+			systemService.updatePasswordById(uid, password);
+		} catch (Exception e) {
+			String message = "#" + ErrorConstants.USER_UPDATE_PASSWORD_SERVICE_ERROR+ "#\n" + 
+					"USER_UPDATE_PASSWORD_SERVICE_ERROR\n" +
+					"[password=" + requestBodyParamMap.get("password") + "]" +
+					"\n";
+			logger.error(message);
+			throw new RestException(HttpStatus.INTERNAL_SERVER_ERROR, message);
+		}
+		
+		result.setStatus(HttpStatus.OK.value());
+		result.setError(HttpStatus.OK.name());
+		
 		return result.getMap();
 	}
 	
+	/**
+	 * 更新用户角色
+	 * @param uid
+	 * @param requestString
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
 	@RequestMapping(value = "/{uid}/role", method = RequestMethod.PUT, produces = MediaTypes.JSON_UTF_8)
 	public Map<String, Object> updateUserRoleFromUserId(
 			@PathVariable("uid") int uid,
-			@RequestBody String requestString
+			@RequestBody String inJson
 			){
-		JSONObject requestJson = JSON.parseObject(requestString);
-		List<Integer> roleIds = (List<Integer>) requestJson.get("ids");
-		User user = UserUtils.get(uid);
-		BasicReturnedJson result = new BasicReturnedJson();
-		if (user != null){
-			List<Role> roleList = new ArrayList<Role>();
-			for (int id : roleIds){
-				if (id>0){
-					roleList.add(new Role(id));
-				}
-			}
-			user.setRoleList(roleList);
-			if (systemService.updateUserRoles(user)){
-				user = UserUtils.get(uid);
-				result.addEntity(getUserMapFromUser(user));
-				result.setStatus(HttpStatus.OK.value());
-				result.setError(HttpStatus.OK.name());
-			}
-			else {
-				result.setStatus(HttpStatus.BAD_REQUEST.value());
-				result.setError(HttpStatus.BAD_REQUEST.name());
-			}
-		}else {
-			result.setStatus(HttpStatus.NOT_FOUND.value());
-			result.setError(HttpStatus.NOT_FOUND.name());
+		Map<String, Object> requestBodyParamMap = ParamUtils.getMapByJson(inJson, logger);
+		List<Integer> roleIds;
+
+		// 输入参数检查
+		try {
+			roleIds = (List<Integer>) requestBodyParamMap.get("ids");
+			
+		} catch(Exception e) {
+			String message = "#" + ErrorConstants.USER_UPDATE_ROLE_PARAM_INVALID + "#\n" + 
+					"USER_UPDATE_ROLE_PARAM_INVALID\n" +
+					"[roleIds=" + requestBodyParamMap.get("ids") + "]" +
+					"\n";
+			logger.error(message);
+			throw new RestException(HttpStatus.BAD_REQUEST, message);
 		}
+				
+		User user = getUserFromId(uid);
+		
+		BasicReturnedJson result = new BasicReturnedJson();
+		
+		List<Role> roleList = new ArrayList<Role>();
+		for (int id : roleIds){
+			if (id>0){
+				roleList.add(new Role(id));
+			}
+		}
+		
+		user.setRoleList(roleList);
+		
+		try {
+			if (!systemService.updateUserRoles(user)) {
+				throw new Exception();
+			}
+		} catch (Exception e) {
+			String message = "#" + ErrorConstants.USER_UPDATE_ROLE_SERVICE_ERROR + "#\n" + 
+					"USER_UPDATE_ROLE_SERVER_ERROR\n" +
+					"\n";
+			logger.error(message);
+			throw new RestException(HttpStatus.INTERNAL_SERVER_ERROR, message);
+		}
+				
+		user = getUserFromId(uid);
+		result.addEntity(getUserMapFromUser(user));
+		result.setStatus(HttpStatus.OK.value());
+		result.setError(HttpStatus.OK.name());
+		
 		return result.getMap();
 	}
 	
+	/**
+	 * 删除用户（逻辑）
+	 * @param uid
+	 * @return
+	 */
 	@RequestMapping(value = "/{uid}", method = RequestMethod.DELETE, produces = MediaTypes.JSON_UTF_8)
 	public Map<String, Object> deleteUserFromUserId(@PathVariable("uid") int uid){
-		User user = UserUtils.get(uid);
+		User user = getUserFromId(uid);
 		BasicReturnedJson result = new BasicReturnedJson();
-		if (user != null){
+
+		try {
 			systemService.deleteUser(user);
-			result.setStatus(HttpStatus.OK.value());
-			result.setError(HttpStatus.OK.name());
-		}else {
-			result.setStatus(HttpStatus.NOT_FOUND.value());
-			result.setError(HttpStatus.NOT_FOUND.name());
+		} catch (Exception e) {
+			String message = "#" + ErrorConstants.USER_DELETE_SERVICE_ERROR + "#\n" + 
+					"USER_DELETE_SERVICE_ERROR\n" +
+					"\n";
+			logger.error(message);
+			throw new RestException(HttpStatus.INTERNAL_SERVER_ERROR, message);
 		}
+		
+		result.setStatus(HttpStatus.OK.value());
+		result.setError(HttpStatus.OK.name());
+		
 		return result.getMap();
 	}
 	
+	
+	/**
+	 * 通过id获取user
+	 * @param uid
+	 * @return
+	 */
+	private User getUserFromId (int uid){
+		User user;
+		try {
+			user = UserUtils.get(uid);
+			if (user == null) {
+				throw new Exception();
+			}
+		} catch (Exception e) {
+			String message = "#" + ErrorConstants.USER_FOR_ID_NOT_FOUND + "#\n" + 
+					"USER_FOR_ID_NOT_FOUND:\n" +
+					"[uid=" + uid + "]" +
+					"\n";
+			logger.error(message);
+			throw new RestException(HttpStatus.BAD_REQUEST, message);
+		}
+		return user;
+	}
+	
+	/**
+	 * user返回格式
+	 * @param user
+	 * @return
+	 */
 	private Map<String, Object> getUserMapFromUser(User user){
 		Map<String, Object> userMap = new HashMap<String, Object>();
 		userMap.put("id", user.getId());
@@ -204,6 +370,11 @@ public class UserController {
 		return userMap;
 	}
 	
+	/**
+	 * Role返回格式
+	 * @param role
+	 * @return
+	 */
 	private Map<String, Object> getRoleMapFromRole(Role role) {
 		Map<String, Object> roleMap = new HashMap<String, Object>();
 		roleMap.put("id", role.getId());
@@ -217,6 +388,11 @@ public class UserController {
 		return roleMap;
 	}
 	
+	/**
+	 * Privilege返回格式
+	 * @param privilege
+	 * @return
+	 */
 	private Map<String, Object> getPrivilegeMapFromPrivilege(Privilege privilege){
 		Map<String, Object> privilegeMap = new HashMap<String, Object>();
 		privilegeMap.put("id", privilege.getId());
