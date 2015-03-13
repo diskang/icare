8bb1704aba21662c2122089dbd94fbd3613578fa
yclli <yclli01@126.com>
2015-03-13 20:41:01 +0800

    add item

---
+++ src/main/java/com/sjtu/icare/modules/gero/persistence/CareItemDAO.java
@@ -0,0 +1,24 @@
+/**
+ * @Package com.sjtu.icare.modules.gero.persistence
+ * @Description TODO
+ * @date Mar 9, 2015 5:18:57 PM
+ * @author Wang Qi
+ * @version TODO
+ */
+package com.sjtu.icare.modules.gero.persistence;
+
+import java.util.List;
+import java.util.Map;
+
+import com.sjtu.icare.common.persistence.annotation.MyBatisDao;
+import com.sjtu.icare.modules.gero.entity.CareItemEntity;
+
+@MyBatisDao
+public interface CareItemDAO {
+
+	CareItemEntity getCareItemEntityById(Map<String, Object> careItemEntity);
+	
+	List<CareItemEntity> getCareItemEntitiesByGeroid(Map<String, Object> careItemEntity);
+	
+	List<CareItemEntity> getCareItemEntitiesByLevel(Map<String, Object> careItemEntity);
+}
