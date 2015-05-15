f6a9d7d3cba43b3ee2791789a227442b4de178fa
garfieldjty <jtygarfield@gmail.com>
2015-05-15 21:07:06 +0800

    message handler

--- src/main/java/com/sjtu/icare/modules/wechat/rule/SubscribeHandler.java
+++ src/main/java/com/sjtu/icare/modules/wechat/rule/SubscribeHandler.java
@@ -2,6 +2,10 @@ package com.sjtu.icare.modules.wechat.rule;
 
 import java.util.Map;
 
+import org.apache.log4j.Logger;
+
+import com.sjtu.icare.common.config.Global;
+
 import me.chanjar.weixin.common.exception.WxErrorException;
 import me.chanjar.weixin.common.session.WxSessionManager;
 import me.chanjar.weixin.mp.api.WxMpMessageHandler;
@@ -11,13 +15,24 @@ import me.chanjar.weixin.mp.bean.WxMpXmlOutMessage;
 import me.chanjar.weixin.mp.bean.WxMpXmlOutTextMessage;
 
 public class SubscribeHandler implements WxMpMessageHandler{
-
+	private static Logger logger = Logger.getLogger(SubscribeHandler.class);
 	@Override
 	public WxMpXmlOutMessage handle(WxMpXmlMessage wxMessage,
 			Map<String, Object> context, WxMpService wxMpService,
 			WxSessionManager sessionManager) throws WxErrorException {
+		String appId = Global.getConfig("wechat.appId");
+		logger.debug(appId);
+		String welcomeMsg = "welcome to us. please click "
+				+ "<a href=\"https://open.weixin.qq.com/connect/oauth2/authorize"
+				+ "?appid="+appId
+				+ "&redirect_uri=http%3A%2F%2F202.12.38.227%2Fwechat%2Ftoken"
+				+ "&response_type=code"
+				+ "&scope=snsapi_userinfo"
+				+ "&state=STATE"
+				+ "#wechat_redirect\">here</a>"
+				+ " to register";
 		WxMpXmlOutTextMessage m
-        = WxMpXmlOutMessage.TEXT().content("welcome to us. please click to register").fromUser(wxMessage.getToUserName())
+        = WxMpXmlOutMessage.TEXT().content(welcomeMsg).fromUser(wxMessage.getToUserName())
         .toUser(wxMessage.getFromUserName()).build();
         return m;
 	}
--- src/main/java/com/sjtu/icare/modules/wechat/utils/RouterConfiguration.java
+++ src/main/java/com/sjtu/icare/modules/wechat/utils/RouterConfiguration.java
@@ -8,6 +8,7 @@ import me.chanjar.weixin.mp.api.WxMpMessageRouter;
 
 import com.sjtu.icare.modules.wechat.rule.DemoLogHandler;
 import com.sjtu.icare.modules.wechat.rule.DemoTextHandler;
+import com.sjtu.icare.modules.wechat.rule.PlatformMessageHandler;
 import com.sjtu.icare.modules.wechat.rule.SubscribeHandler;
 
 public class RouterConfiguration {
@@ -17,10 +18,14 @@ public class RouterConfiguration {
     	WxMpMessageHandler logHandler = new DemoLogHandler();
     	WxMpMessageHandler textHandler = new DemoTextHandler();
     	WxMpMessageHandler subscribeHandler = new SubscribeHandler();
+    	WxMpMessageHandler platformMessageHandler = new PlatformMessageHandler();
     	wxMpMessageRouter.rule().handler(logHandler).next()
     	.rule().async(false).content("test").handler(textHandler).end()
     	.rule().async(false).msgType(WxConsts.XML_MSG_EVENT)
-    	       .event(WxConsts.EVT_SUBSCRIBE).handler(subscribeHandler).end();
+    	       .event(WxConsts.EVT_SUBSCRIBE).handler(subscribeHandler).end()
+    	.rule().async(false).msgType(WxConsts.XML_MSG_EVENT)
+    	       .event(WxConsts.EVT_CLICK).eventKey("sys_notice").handler(platformMessageHandler).end()
+    	       ;
     	logger.warn("router handler configured");
 	}
 }
