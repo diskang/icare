706def9591c2c75491a012e020f3eb0b20885930
garfieldjty <jtygarfield@gmail.com>
2015-05-22 14:22:57 +0800

    add three health data api for detailed query in wechat

--- src/main/java/com/sjtu/icare/modules/wechat/webservice/WechatElderController.java
+++ src/main/java/com/sjtu/icare/modules/wechat/webservice/WechatElderController.java
@@ -1,5 +1,6 @@
 package com.sjtu.icare.modules.wechat.webservice;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -9,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,11 +19,18 @@ import org.springframework.web.bind.annotation.RestController;
 import com.sjtu.icare.common.config.CommonConstants;
 import com.sjtu.icare.common.config.ErrorConstants;
 import com.sjtu.icare.common.utils.BasicReturnedJson;
+import com.sjtu.icare.common.utils.DateUtils;
 import com.sjtu.icare.common.web.rest.BasicController;
 import com.sjtu.icare.common.web.rest.MediaTypes;
 import com.sjtu.icare.common.web.rest.RestException;
+import com.sjtu.icare.modules.elder.entity.ElderBloodPressureEntity;
+import com.sjtu.icare.modules.elder.entity.ElderHeartRateEntity;
+import com.sjtu.icare.modules.elder.entity.ElderTemperatureEntity;
+import com.sjtu.icare.modules.elder.service.IElderHealthDataService;
 import com.sjtu.icare.modules.elder.service.IElderInfoService;
 import com.sjtu.icare.modules.sys.entity.User;
+import com.sjtu.icare.modules.wechat.service.IElderRelativeRelationshipService;
+import com.sjtu.icare.modules.wechat.service.impl.ElderRelativeRelationshipService;
 
 /*
  * not used
@@ -34,6 +43,10 @@ public class WechatElderController extends BasicController{
 	
 	@Autowired
 	private IElderInfoService elderInfoService;
+	@Autowired 
+	private IElderRelativeRelationshipService elderRelativeRelationshipService;
+	@Autowired 
+	private IElderHealthDataService elderHealthDataService;
 	
 	@RequestMapping(method = RequestMethod.GET, produces = MediaTypes.JSON_UTF_8)
 	public Object getElderListByNameAndGero(
@@ -69,5 +82,132 @@ public class WechatElderController extends BasicController{
 			logger.error(message);
 			throw new RestException(HttpStatus.INTERNAL_SERVER_ERROR, message);
 		}
+	}
+	
+	@RequestMapping(value="/{eid}/heart_rate",method = RequestMethod.GET, produces = MediaTypes.JSON_UTF_8)
+	public Object getElderHeartRate(
+			HttpServletRequest request,
+			@PathVariable("eid") int elderId,
+//			@RequestParam(value="start_date", required=false) String startDate,
+//			@RequestParam(value="end_date", required=false) String endDate,
+			@RequestParam(value="wechat_id", required=false)String wechatId
+			) 
+	{
+		if (wechatId==null || wechatId.isEmpty()) {
+			throw new RestException(HttpStatus.BAD_REQUEST, "wechat Id required"); 
+		}
+		
+		//get elderInfos from openId
+		ElderRelativeRelationshipService.ElderRelativeRelationshipReturn
+		relativeElders = elderRelativeRelationshipService.getElderRelativeRelationshipsByWechatId(wechatId);
+		if(relativeElders==null){//user not register, redirect to register page
+			throw new RestException(HttpStatus.UNAUTHORIZED, "user not register");
+		}
+		String status = (String)relativeElders.getStatus();
+		if(! CommonConstants.SUBSCRIBED_WITH_RELATIONSHIP_BINDING.equals(status)){
+			throw new RestException(HttpStatus.UNAUTHORIZED, "no elders bind");
+		}
+		
+		try{
+			ElderHeartRateEntity queryHeartRateEntity = new ElderHeartRateEntity();
+			queryHeartRateEntity.setElderId(elderId);
+			String endDate = DateUtils.formatDate(DateUtils.addDays(new Date(), 1),"yyyy-MM-dd");
+			String startDate = DateUtils.formatDate(DateUtils.addDays(new Date(), -30),"yyyy-MM-dd");
+			List<ElderHeartRateEntity> elderHeartRateEntityList = 
+					elderHealthDataService.getElderHeartRateEntity(
+					queryHeartRateEntity, startDate, endDate);
+			BasicReturnedJson basicReturnedJson = new BasicReturnedJson();
+			for(ElderHeartRateEntity entity:elderHeartRateEntityList){
+				basicReturnedJson.addEntity(entity);
+			}
+			return basicReturnedJson.getMap();
+		}catch(Exception e){
+//			logger.error("");
+			e.printStackTrace();
+			throw new RestException(HttpStatus.INTERNAL_SERVER_ERROR, "cannot get heart rate"); 
+		}
+	}
+	
+	@RequestMapping(value="/{eid}/blood_pressure",method = RequestMethod.GET, produces = MediaTypes.JSON_UTF_8)
+	public Object getElderBloodPressure(
+			HttpServletRequest request,
+			@PathVariable("eid") int elderId,
+//			@RequestParam(value="start_date", required=false) String startDate,
+//			@RequestParam(value="end_date", required=false) String endDate,
+			@RequestParam(value="wechat_id", required=false)String wechatId
+			) {
+		if (wechatId==null || wechatId.isEmpty()) {
+			throw new RestException(HttpStatus.BAD_REQUEST, "wechat Id required"); 
+		}
+		ElderRelativeRelationshipService.ElderRelativeRelationshipReturn
+		relativeElders = elderRelativeRelationshipService.getElderRelativeRelationshipsByWechatId(wechatId);
+		if(relativeElders==null){//user not register, redirect to register page
+			throw new RestException(HttpStatus.UNAUTHORIZED, "user not register");
+		}
+		String status = (String)relativeElders.getStatus();
+		if(! CommonConstants.SUBSCRIBED_WITH_RELATIONSHIP_BINDING.equals(status)){
+			throw new RestException(HttpStatus.UNAUTHORIZED, "no elders bind");
+		}
+		try {
+			ElderBloodPressureEntity queryBloodPressureEntity = new ElderBloodPressureEntity();
+			queryBloodPressureEntity.setElderId(elderId);
+			
+			String endDate = DateUtils.formatDate(DateUtils.addDays(new Date(), 1),"yyyy-MM-dd");
+			//get data for last 30 days
+			String startDate = DateUtils.formatDate(DateUtils.addDays(new Date(), -30),"yyyy-MM-dd");
+			List<ElderBloodPressureEntity> elderBloodPressureEntityList = 
+					elderHealthDataService.getElderBloodPressureEntities(
+					queryBloodPressureEntity, startDate, endDate);
+			BasicReturnedJson basicReturnedJson = new BasicReturnedJson();
+			for(ElderBloodPressureEntity entity: elderBloodPressureEntityList){
+				basicReturnedJson.addEntity(entity);
+			}
+			return basicReturnedJson.getMap();
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RestException(HttpStatus.INTERNAL_SERVER_ERROR, "cannot get blood pressure"); 
+		}
+		
+	}
+	
+	@RequestMapping(value="/{eid}/temperature",method = RequestMethod.GET, produces = MediaTypes.JSON_UTF_8)
+	public Object getElderTemperature(
+			HttpServletRequest request,
+			@PathVariable("eid") int elderId,
+//			@RequestParam(value="start_date", required=false) String startDate,
+//			@RequestParam(value="end_date", required=false) String endDate,
+			@RequestParam(value="wechat_id", required=false)String wechatId
+			) {
+		if (wechatId==null || wechatId.isEmpty()) {
+			throw new RestException(HttpStatus.BAD_REQUEST, "wechat Id required"); 
+		}
+		ElderRelativeRelationshipService.ElderRelativeRelationshipReturn
+		relativeElders = elderRelativeRelationshipService.getElderRelativeRelationshipsByWechatId(wechatId);
+		if(relativeElders==null){//user not register, redirect to register page
+			throw new RestException(HttpStatus.UNAUTHORIZED, "user not register");
+		}
+		String status = (String)relativeElders.getStatus();
+		if(! CommonConstants.SUBSCRIBED_WITH_RELATIONSHIP_BINDING.equals(status)){
+			throw new RestException(HttpStatus.UNAUTHORIZED, "no elders bind");
+		}
+		try {
+			BasicReturnedJson basicReturnedJson = new BasicReturnedJson();
+			ElderTemperatureEntity queryTemperatureEntity = new ElderTemperatureEntity();
+			queryTemperatureEntity.setElderId(elderId);
+			String endDate = DateUtils.formatDate(DateUtils.addDays(new Date(), 1),"yyyy-MM-dd");
+			//get data for last 30 days
+			String startDate = DateUtils.formatDate(DateUtils.addDays(new Date(), -30),"yyyy-MM-dd");
+			List<ElderTemperatureEntity> elderTemperatureEntityList = 
+					elderHealthDataService.getElderTemperatureEntities(
+					queryTemperatureEntity, startDate, endDate);
+			for(ElderTemperatureEntity entity:elderTemperatureEntityList){
+				basicReturnedJson.addEntity(entity);
+			}
+			return basicReturnedJson.getMap();
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RestException(HttpStatus.INTERNAL_SERVER_ERROR, "cannot get temperature"); 
+		}
+		
 	}
 }
